92c1e5a9a9bf84597f384a9d6d120b1a3cbe73ad
Arthur Ricardo <arthur.ricardosouza2000@gmail.com>
2022-08-19 14:05:17 -0400

    Commit 8

--- AprenderJS/JS6.js
+++ AprenderJS/JS6.js
@@ -14,8 +14,25 @@ function soma(valor1, valor2){
 
 document.getElementById("texto").innerHTML = soma(10,20); //innerHTML : passa uma mensagem pro documento html que esta sendo substituido.
 var total = soma(50,78);
-alert (total);//estou passando a mensagem da função pelo o alert.
-console.log("Oi sou o console.log();");//estou imprimindo uma mensagem no console.
+//alert (total);// estou passando a mensagem da função pelo o alert. 
+console.log("Oi sou o console.log()");//estou imprimindo uma mensagem no console.
+var num =5;
+{
+   var num=10;
+}
+console.log(num);
+let afe= 8;
+{
+   let afe =16;
+
+} 
+console.log(afe);//aqui vai ser 8 porque o LET ja foi declarado.
+
+let valor1 =5
+let valor2 =15
+var total = (valor1 == valor2)
+total -(valor1 < 2);
+alert (total);
 
 
 
