3082b86006fb294694ef6ffd05da7526c686eb36
Arthur Ricardo <arthur.ricardosouza2000@gmail.com>
2022-09-24 21:40:50 -0400

    Commit 9
    Organizando os arquivos

---
+++ HTML/Atividade/atividade.js
@@ -0,0 +1,13 @@
+/*1 - CRIAR 3 CAIXAS DE TEXTO DO TIPO NUMERO E APRESENTAR O MAIOR VALOR EM UMA OUTRA CAIXA DE TEXTO DO TIPO NUMERO E APRESENTAR
+2 - VALIDAR UM FORMULARIO COM OS SEGUINTES CAMPOS
+    -INPUT TEXT
+    -INPUT OPTION
+    -INPUT CHECKBOX
+3 - CRIAR UMA TABELA DE TAMANHO 3X3. CRIAR UM BOTAO "GERAR NUMEROS". NA TABELA APARECER OS NUMEROS GERADOS DE FORMA RANDOMICA
+*/ 
+/*
+1- Criar um menu lateral com 5 itens. Criar o famoso "hamburguer" para mostrar e esconder  o menu.
+2-Criar 2 comboboxes e prencher-los com numeros pares de 1 a 50 e o outro, com impares de 50-100.
+3-Criar um combo para selecionar o estado (3 estados) e depois de selecionar um estado 
+mostrar as cidades do estado selecionando.(3 cidades). 
+*/
---
+++ HTML/ProjetosHTML,CSS/Calculadora/calcu.js
@@ -0,0 +1,7 @@
+console.log('Site de calculadora');
+
+let a =40;
+var b = 80;
+
+console.log(a);
+console.log(b);
