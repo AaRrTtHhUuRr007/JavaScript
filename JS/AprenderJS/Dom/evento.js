d5d767e67cc8f5553b1c3bd2a2bc7ae08488d0eb
Arthur Ricardo <arthur.ricardosouza2000@gmail.com>
2023-11-06 07:52:34 -0400

    Commit 05/11/2023

---
+++ JS/AprenderJS/Dom/evento.js
@@ -0,0 +1,17 @@
+/*Evento
+const evento1 =document.getElementById('e1')
+
+evento1.addEventListener('click',()=>{
+    alert('clique')
+})
+*/
+
+const caixa1 =document.querySelector("#caixa1")
+const caixa2 =document.querySelector("#caixa2")
+const copiar =document.querySelector('#copiar')
+const todosCursos =[...document.querySelectorAll('.curso')]
+
+
+console.log(todosCursos)
+
+
